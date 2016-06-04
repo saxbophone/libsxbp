34c1d37cb9c4707d430916d56b32a9a5ba0c4d23
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-06-04 18:50:50 +0100

    Move out render_spiral() function from program code to library code

--- renderer.c
+++ renderer.c
@@ -1,21 +1,12 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
 
 #include <png.h>
 
 #include "saxbospiral/saxbospiral.h"
-#include "saxbospiral/plot.h"
 #include "saxbospiral/render.h"
 #include "saxbospiral/serialise.h"
 
-
-typedef struct bitmap_t {
-    uint64_t width;
-    uint64_t height;
-    bool ** pixels;
-} bitmap_t;
 
 // prints out usage information to a given FILE handle
 static void
@@ -43,102 +34,6 @@ file_size(FILE * file_handle) {
     // seek to start again
     fseek(file_handle, 0L, SEEK_SET);
     return file_size;
-}
-
-// given a spiral struct with co-ords in it's cache, find and return the co-ords
-// for the corners of the square needed to contain the points.
-static co_ord_array_t
-get_bounds(spiral_t spiral) {
-    int64_t min_x = 0;
-    int64_t min_y = 0;
-    int64_t max_x = 0;
-    int64_t max_y = 0;
-    for(size_t i = 0; i < spiral.co_ord_cache.co_ords.size; i++) {
-        if(spiral.co_ord_cache.co_ords.items[i].x < min_x) {
-            min_x = spiral.co_ord_cache.co_ords.items[i].x;
-        }
-        if(spiral.co_ord_cache.co_ords.items[i].y < min_y) {
-            min_y = spiral.co_ord_cache.co_ords.items[i].y;
-        }
-        if(spiral.co_ord_cache.co_ords.items[i].x > max_x) {
-            max_x = spiral.co_ord_cache.co_ords.items[i].x;
-        }
-        if(spiral.co_ord_cache.co_ords.items[i].y > max_y) {
-            max_y = spiral.co_ord_cache.co_ords.items[i].y;
-        }
-    }
-    // initialise output struct
-    co_ord_array_t bounds = {
-        .size = 2,
-        .items = calloc(sizeof(co_ord_t), 2),
-    };
-    // write bounds to struct
-    bounds.items[0].x = min_x;
-    bounds.items[0].y = min_y;
-    bounds.items[1].x = max_x;
-    bounds.items[1].y = max_y;
-    return bounds;
-}
-
-// given a spiral struct, returns a bitmap_t representing a monochromatic image
-// of the rendered spiral
-static bitmap_t
-render_spiral(spiral_t spiral) {
-    // plot co-ords of spiral into it's cache
-    cache_spiral_points(&spiral, spiral.size);
-    // get the min and max bounds of the spiral's co-ords
-    co_ord_array_t bounds = get_bounds(spiral);
-    // get the normalisation vector needed to make all values unsigned
-    tuple_t normalisation_vector = {
-        .x = -bounds.items[0].x,
-        .y = -bounds.items[0].y,
-    };
-    // get co-ords of top left and bottom right corners, as unsigned
-    co_ord_t top_left = {
-        .x = 0,
-        .y = 0,
-    };
-    co_ord_t bottom_right = {
-        .x = bounds.items[1].x + normalisation_vector.x,
-        .y = bounds.items[1].y + normalisation_vector.y,
-    };
-    // initialise output bitmap - image dimensions are twice the size + 1
-    bitmap_t output = {
-        .width = ((bottom_right.x + 1) * 2) + 1,
-        .height = ((bottom_right.y + 1) * 2) + 1,
-    };
-    // allocate dynamic memory - 2D array of bools
-    output.pixels = malloc(output.width * sizeof(bool*));
-    for(size_t i = 0; i < output.width; i++) {
-        output.pixels[i] = calloc(output.height, sizeof(bool));
-    }
-    // set 'current point' co-ordinate
-    co_ord_t current = {
-        .x = 0,
-        .y = 0,
-    };
-    // plot the lines of the spiral as points
-    for(size_t i = 0; i < spiral.size; i++) {
-        // get current direction
-        vector_t direction = VECTOR_DIRECTIONS[spiral.lines[i].direction];
-        // make as many jumps in this direction as this lines length
-        for(uint64_t j = 0; j < (spiral.lines[i].length * 2) + 1; j++) {
-            // get output co-ords
-            int64_t x_pos = current.x + (normalisation_vector.x * 2) + 1;
-            int64_t y_pos = current.y + (normalisation_vector.y * 2) + 1;
-            // skip the second pixel of the first line
-            if(!((i == 0) && (j == 1))) {
-                // flip the y-axis otherwise they appear vertically mirrored
-                output.pixels[x_pos][output.height - 1 - y_pos] = true;
-            }
-            if(j != (spiral.lines[i].length * 2)) {
-                // if we're not on the last line, advance the marker along
-                current.x += direction.x;
-                current.y += direction.y;
-            }
-        }
-    }
-    return output;
 }
 
 // given a file handle and a bitmap_t struct, write the bitmap data as a PNG
--- saxbospiral/render.h
+++ saxbospiral/render.h
@@ -1,14 +1,28 @@
 #ifndef SAXBOPHONE_SAXBOSPIRAL_RENDER_H
 #define SAXBOPHONE_SAXBOSPIRAL_RENDER_H
 
-#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "saxbospiral.h"
 
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-/* Code goes here... */
+typedef struct bitmap_t {
+    uint64_t width;
+    uint64_t height;
+    bool ** pixels;
+} bitmap_t;
+
+/*
+ * given a spiral struct, returns a bitmap_t representing a monochromatic image
+ * of the rendered spiral
+ */
+bitmap_t
+render_spiral(spiral_t spiral);
 
 #ifdef __cplusplus
 } // extern "C"
