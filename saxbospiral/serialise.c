ab5049a1306203191d57bec265249847943c0b40
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-08-13 17:52:43 +0100

    Fix small bug in serialise dump function Not really so much as a badly impacting bug but one that may have an impact in rare scenarios.

--- saxbospiral/serialise.c
+++ saxbospiral/serialise.c
@@ -103,7 +103,7 @@ dump_spiral(spiral_t spiral) {
     for(uint8_t i = 0; i < 8; i++) {
         uint8_t shift = (8 * (7 - i));
         output.bytes[16 + i] = (uint8_t)(
-            ((uint64_t)spiral.size & (0xff << shift)) >> shift
+            ((uint64_t)spiral.size & (0xffUL << shift)) >> shift
         );
     }
     // write final newline at end of header
