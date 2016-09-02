b72b737cd351dc02a0bce5f00354bc0d0d37ae6d
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-09-03 00:08:30 +0100

    Amend functional test and build command for new cli

--- build_logo.sh
+++ build_logo.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # 
 # Script for updating the project logo
-# The paths of the prepare, generate and render commands are
-# passed as the first 3 arguments. The 4th argument is the
-# file to write the PNG output to and the 5th is the message
-# to use for the logo data (of format "saxbospiral vX.Y.Z")
+# The first argument is the path to the sxp cli program.
+# The second argument is the file to write the PNG output to.
+# The third is the message to use for the logo data
+# (of format "saxbospiral vX.Y.Z").
 # 
 echo "Generating logo";
-echo -n "$5" > temp.hex && \
-./"$1" temp.hex temp.sxp && \
-./"$2" temp.sxp && \
-./"$3" temp.sxp "$4" && \
+echo -n "$3" > temp.hex && \
+./"$1" -pgr -i temp.hex -o "$2"
 rm temp.hex temp.sxp;
--- func_test.sh
+++ func_test.sh
@@ -2,10 +2,9 @@
 # 
 # Functional test script.
 # Generates a new logo and compares with the current one.
-# The paths of the prepare, generate and render commands are
-# passed as the first 3 arguments. The 4th argument is the
-# message to use for the file (see build_logo.sh)
+# The first argument is the path to the sxp cli program.
+# The second argument is the message to use for the file (see build_logo.sh)
 # 
-./build_logo.sh "$1" "$2" "$3" "test_saxbospiral.png" "$4" && \
+./build_logo.sh "$1" "test_saxbospiral.png" "$2" && \
 diff "saxbospiral.png" "test_saxbospiral.png" && \
 rm "test_saxbospiral.png";
