a306394ce00790189b26e01675656935221ebfbd
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-10-20 20:42:04 +0100

    Licensed all the source files under the AGPL License v3.0

--- saxbospiral/initialise.c
+++ saxbospiral/initialise.c
@@ -1,3 +1,26 @@
+/*
+ * This source file forms part of libsaxbospiral, a library which generates
+ * experimental 2D spiral-like shapes based on input binary data.
+ *
+ * This compilation unit provides basic functions to initialise a spiral.
+ *
+ *
+ *
+ * Copyright (C) 2016, Joshua Saxby joshua.a.saxby+TNOPLuc8vM==@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdint.h>
 #include <stdlib.h>
 
--- saxbospiral/serialise.h
+++ saxbospiral/serialise.h
@@ -1,3 +1,27 @@
+/*
+ * This source file forms part of libsaxbospiral, a library which generates
+ * experimental 2D spiral-like shapes based on input binary data.
+ *
+ * This compilation unit provides functions for serialising spirals to and from
+ * byte sequences (which may be stored in files or any other form).
+ *
+ *
+ *
+ * Copyright (C) 2016, Joshua Saxby joshua.a.saxby+TNOPLuc8vM==@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef SAXBOPHONE_SAXBOSPIRAL_SERIALISE_H
 #define SAXBOPHONE_SAXBOSPIRAL_SERIALISE_H
 
