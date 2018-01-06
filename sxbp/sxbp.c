59685c7cd861c87efbaafbae8c4c0c32944ccf5f
Joshua Saxby <joshua.a.saxby@gmail.com>
2018-01-06 03:22:27 +0000

    Add new skeleton base compilation unit

---
+++ sxbp/sxbp.c
@@ -0,0 +1,22 @@
+/*
+ * This source file forms part of libsxbp, a library which generates
+ * experimental 2D spiral-like shapes based on input binary data.
+ *
+ * Copyright (C) Joshua Saxby <joshua.a.saxby@gmail.com> 2016-2017, 2018
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/* INCLUDES GO HERE */
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/* CODE GOES HERE */
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
---
+++ sxbp/sxbp.h
@@ -0,0 +1,40 @@
+/*
+ * This source file forms part of libsxbp, a library which generates
+ * experimental 2D spiral-like shapes based on input binary data.
+ */
+
+/**
+ * @file
+ *
+ * @brief This compilation unit provides the public data types and symbols
+ * exposed by the library.
+ *
+ * @author Joshua Saxby <joshua.a.saxby@gmail.com>
+ * @date 2018
+ *
+ * @copyright Copyright (C) Joshua Saxby 2016-2017, 2018
+ *
+ * @copyright
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// include guard
+#ifndef SAXBOPHONE_SXBP_SXBP_H
+#define SAXBOPHONE_SXBP_SXBP_H
+
+/* INCLUDES GO HERE */
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/* CODE GOES HERE */
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+// end of header file
+#endif
