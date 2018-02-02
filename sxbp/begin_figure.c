6ac84c37c96ebb5bde3abea295b89be873672649
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2018-02-02 22:35:05 +0000

    Replace one missed out bare uint32_t type

--- sxbp/begin_figure.c
+++ sxbp/begin_figure.c
@@ -109,7 +109,8 @@ static void sxbp_plot_lines(const sxbp_buffer_t* data, sxbp_figure_t* figure) {
             // bit level loop - extract the bit
             uint8_t e = 7 - b; // which power of two to use with bit mask
             bool bit = (data->bytes[s] & (1 << e)) >> e; // the current bit
-            sxbp_figure_size_t index = (s * 8) + (uint32_t)b + 1; // line index
+            // this is the line index, derived from the bit of data we're on
+            sxbp_figure_size_t index = (s * 8) + (sxbp_figure_size_t)b + 1;
             // set rotation direction based on the current bit
             sxbp_rotation_t rotation = sxbp_rotation_from_bit(bit);
             // calculate the new direction
