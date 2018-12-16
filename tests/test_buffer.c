626eeda3dd4041c0525f8630d4e394e03f417291
Joshua Saxby <joshua.a.saxby@gmail.com>
2018-12-16 16:41:20 +0000

    Wrote unit tests for buffer, up to freeing function

--- tests/test_buffer.c
+++ tests/test_buffer.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stddef.h>
 
 #include <check.h>
@@ -38,6 +39,40 @@ START_TEST(test_init_buffer_null) {
     ck_assert(result == SXBP_RESULT_FAIL_PRECONDITION);
 } END_TEST
 
+START_TEST(test_free_buffer_unallocated) {
+    sxbp_buffer_t buffer = sxbp_blank_buffer();
+
+    /*
+     * it should be possible to safely call the freeing function on an
+     * unallocated buffer
+     */
+    bool needed_free = sxbp_free_buffer(&buffer);
+
+    // no memory should be pointed to
+    ck_assert_ptr_null(buffer.bytes);
+    // the function should return false to tell us it didn't need to free
+    ck_assert(!needed_free);
+} END_TEST
+
+START_TEST(test_free_buffer_allocated) {
+    sxbp_buffer_t buffer = { .size = 10000, .bytes = NULL, };
+    /*
+     * allocate the buffer -if this fails then we'll abort here because this
+     * test case is not testing the init function
+     */
+    if (sxbp_init_buffer(&buffer) != SXBP_RESULT_OK) {
+        ck_abort_msg("Unable to allocate buffer");
+    }
+
+    // calling the freeing function on this allocated buffer should free memory
+    bool needed_free = sxbp_free_buffer(&buffer);
+
+    // no memory should be pointed to
+    ck_assert_ptr_null(buffer.bytes);
+    // the function should return true to tell us it needed to free
+    ck_assert(needed_free);
+} END_TEST
+
 Suite* make_buffer_suite(void) {
     // Test cases for buffer data type
     Suite* test_suite = suite_create("Buffer");
@@ -55,6 +90,14 @@ Suite* make_buffer_suite(void) {
     );
     tcase_add_test(init_buffer_null, test_init_buffer_null);
     suite_add_tcase(test_suite, init_buffer_null);
+
+    TCase* free_buffer_unallocated = tcase_create("Free an unallocated Buffer");
+    tcase_add_test(free_buffer_unallocated, test_free_buffer_unallocated);
+    suite_add_tcase(test_suite, free_buffer_unallocated);
+
+    TCase* free_buffer_allocated = tcase_create("Free an allocated Buffer");
+    tcase_add_test(free_buffer_allocated, test_free_buffer_allocated);
+    suite_add_tcase(test_suite, free_buffer_allocated);
 
     return test_suite;
 }
