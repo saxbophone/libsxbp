aab30f5df6a4478ca23ec093001e6a81633b4ac9
Joshua Saxby <joshua.a.saxby@gmail.com>
2018-12-16 06:39:23 +0000

    Amend variable naming

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -7,11 +7,11 @@
 
 int main(void) {
     int number_failed = -1;
-    SRunner* sr = srunner_create(make_bitmap_suite());
-    srunner_add_suite(sr, make_buffer_suite());
+    SRunner* suite_runner = srunner_create(make_bitmap_suite());
+    srunner_add_suite(suite_runner, make_buffer_suite());
 
-    srunner_run_all(sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
+    srunner_run_all(suite_runner, CK_NORMAL);
+    number_failed = srunner_ntests_failed(suite_runner);
+    srunner_free(suite_runner);
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
