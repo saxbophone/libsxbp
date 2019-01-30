5894835e6d8e8ba795c2a06f2519ecc0c87cbb63
Joshua Saxby <joshua.a.saxby@gmail.com>
2019-01-30 02:38:11 +0000

    Completed inclusion of all figure test cases in test suite Note: Still no actual test cases are implemented!

--- tests/test_figure.c
+++ tests/test_figure.c
@@ -168,95 +168,88 @@ START_TEST(test_render_figure_to_svg) {
     ck_abort_msg("Test not implemented!");
 } END_TEST
 
+START_TEST(test_render_figure_to_svg_figure_null) {
+    // TODO: replace this body with actual test code
+    ck_abort_msg("Test not implemented!");
+} END_TEST
+
+START_TEST(test_render_figure_to_svg_buffer_null) {
+    // TODO: replace this body with actual test code
+    ck_abort_msg("Test not implemented!");
+} END_TEST
+
 Suite* make_figure_suite(void) {
     // Test cases for figure data type
     Suite* test_suite = suite_create("Figure");
 
-    TCase* blank_figure = tcase_create("Create blank Figure");
+    TCase* blank_figure = tcase_create("sxbp_blank_figure()");
     tcase_add_test(blank_figure, test_blank_figure);
     suite_add_tcase(test_suite, blank_figure);
 
-    TCase* init_figure = tcase_create("Allocate a Figure");
+    TCase* init_figure = tcase_create("sxbp_init_figure()");
     tcase_add_test(init_figure, test_init_figure);
+    tcase_add_test(init_figure, test_init_figure_null);
     suite_add_tcase(test_suite, init_figure);
 
-    TCase* init_figure_null = tcase_create(
-        "Figure allocation returns appropriate error code when given NULL pointer"
-    );
-    tcase_add_test(init_figure_null, test_init_figure_null);
-    suite_add_tcase(test_suite, init_figure_null);
+    TCase* free_figure = tcase_create("sxbp_free_figure()");
+    tcase_add_test(free_figure, test_free_figure_unallocated);
+    tcase_add_test(free_figure, test_free_figure_allocated);
+    suite_add_tcase(test_suite, free_figure);
 
-    TCase* free_figure_unallocated = tcase_create("Free an unallocated Figure");
-    tcase_add_test(free_figure_unallocated, test_free_figure_unallocated);
-    suite_add_tcase(test_suite, free_figure_unallocated);
-
-    TCase* free_figure_allocated = tcase_create("Free an allocated Figure");
-    tcase_add_test(free_figure_allocated, test_free_figure_allocated);
-    suite_add_tcase(test_suite, free_figure_allocated);
-
-    TCase* copy_figure = tcase_create("Copy a figure");
+    TCase* copy_figure = tcase_create("sxbp_copy_figure()");
     tcase_add_test(copy_figure, test_copy_figure);
+    tcase_add_test(copy_figure, test_copy_figure_from_null);
+    tcase_add_test(copy_figure, test_copy_figure_to_null);
     suite_add_tcase(test_suite, copy_figure);
 
-    TCase* copy_figure_from_null = tcase_create(
-        "Figure copying returns appropriate error code when from is NULL"
-    );
-    tcase_add_test(copy_figure_from_null, test_copy_figure_from_null);
-    suite_add_tcase(test_suite, copy_figure_from_null);
-
-    TCase* copy_figure_to_null = tcase_create(
-        "Figure copying returns appropriate error code when to is NULL"
-    );
-    tcase_add_test(copy_figure_to_null, test_copy_figure_to_null);
-    suite_add_tcase(test_suite, copy_figure_to_null);
-
-    TCase* begin_figure = tcase_create("Begin generation of a figure");
+    TCase* begin_figure = tcase_create("sxbp_begin_figure()");
     tcase_add_test(begin_figure, test_begin_figure);
+    tcase_add_test(begin_figure, test_begin_figure_data_too_big);
+    tcase_add_test(begin_figure, test_begin_figure_data_null);
+    tcase_add_test(begin_figure, test_begin_figure_figure_null);
     suite_add_tcase(test_suite, begin_figure);
 
-    TCase* begin_figure_data_too_big = tcase_create(
-        "Figure generation returns appropriate error code when given data that "
-        "is too large"
-    );
-    tcase_add_test(begin_figure_data_too_big, test_begin_figure_data_too_big);
-    suite_add_tcase(test_suite, begin_figure_data_too_big);
+    TCase* refine_figure = tcase_create("sxbp_refine_figure()");
+    tcase_add_test(refine_figure, test_refine_figure);
+    tcase_add_test(refine_figure, test_refine_figure_figure_null);
+    tcase_add_test(refine_figure, test_refine_figure_no_lines);
+    tcase_add_test(refine_figure, test_refine_figure_unimplemented_method);
+    suite_add_tcase(test_suite, refine_figure);
 
-    TCase* begin_figure_data_null = tcase_create(
-        "Figure generation returns appropriate error code when data is NULL"
-    );
-    tcase_add_test(begin_figure_data_null, test_begin_figure_data_null);
-    suite_add_tcase(test_suite, begin_figure_data_null);
+    TCase* dump_figure = tcase_create("sxbp_dump_figure()");
+    tcase_add_test(dump_figure, test_dump_figure);
+    tcase_add_test(dump_figure, test_dump_figure_figure_null);
+    tcase_add_test(dump_figure, test_dump_figure_buffer_null);
+    suite_add_tcase(test_suite, dump_figure);
 
-    TCase* begin_figure_figure_null = tcase_create(
-        "Figure generation returns appropriate error code when figure is NULL"
-    );
-    tcase_add_test(begin_figure_figure_null, test_begin_figure_figure_null);
-    suite_add_tcase(test_suite, begin_figure_figure_null);
+    TCase* load_figure = tcase_create("sxbp_load_figure()");
+    tcase_add_test(load_figure, test_load_figure);
+    tcase_add_test(load_figure, test_load_figure_buffer_null);
+    tcase_add_test(load_figure, test_load_figure_figure_null);
+    suite_add_tcase(test_suite, load_figure);
 
-    /*
-     * TODO: Add these test cases:
-     *
-     * - test_refine_figure
-     * - test_refine_figure_figure_null
-     * - test_refine_figure_no_lines
-     * - test_refine_figure_unimplemented_method
-     * - test_dump_figure
-     * - test_dump_figure_figure_null
-     * - test_dump_figure_buffer_null
-     * - test_load_figure
-     * - test_load_figure_buffer_null
-     * - test_load_figure_figure_null
-     * - test_render_figure
-     * - test_render_figure_figure_null
-     * - test_render_figure_buffer_null
-     * - test_render_figure_render_callback_null
-     * - test_render_figure_to_null
-     * - test_render_figure_to_pbm
-     * - test_render_figure_to_pbm_figure_null
-     * - test_render_figure_to_pbm_buffer_null
-     * - test_render_figure_to_sv
-     *
-     */
+    TCase* render_figure = tcase_create("sxbp_render_figure()");
+    tcase_add_test(render_figure, test_render_figure);
+    tcase_add_test(render_figure, test_render_figure_figure_null);
+    tcase_add_test(render_figure, test_render_figure_buffer_null);
+    tcase_add_test(render_figure, test_render_figure_render_callback_null);
+    suite_add_tcase(test_suite, render_figure);
+
+    TCase* render_figure_to_null = tcase_create("sxbp_render_figure_to_null()");
+    tcase_add_test(render_figure_to_null, test_render_figure_to_null);
+    suite_add_tcase(test_suite, render_figure_to_null);
+
+    TCase* render_figure_to_pbm = tcase_create("sxbp_render_figure_to_pbm()");
+    tcase_add_test(render_figure_to_pbm, test_render_figure_to_pbm);
+    tcase_add_test(render_figure_to_pbm, test_render_figure_to_pbm_figure_null);
+    tcase_add_test(render_figure_to_pbm, test_render_figure_to_pbm_buffer_null);
+    suite_add_tcase(test_suite, render_figure_to_pbm);
+
+    TCase* render_figure_to_svg = tcase_create("sxbp_render_figure_to_svg()");
+    tcase_add_test(render_figure_to_svg, test_render_figure_to_svg);
+    tcase_add_test(render_figure_to_svg, test_render_figure_to_svg_figure_null);
+    tcase_add_test(render_figure_to_svg, test_render_figure_to_svg_buffer_null);
+    suite_add_tcase(test_suite, render_figure_to_svg);
 
     return test_suite;
 }
